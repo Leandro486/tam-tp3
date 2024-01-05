3aa00b1729f65309ae3321bbc62c96d84a527c25
Leandro486 <duarte.nazz@gmail.com>
2024-01-05 16:19:36 +0000

    Update index.py

--- index.py
+++ index.py
@@ -118,9 +118,11 @@ def verifyUti():
                         with conn.cursor() as cursor:
                             cursor.execute(update_token, values_token)
                         conn.commit()
+                        return jsonify({"uti_id": rows[0][0], "uti_login": rows[0][1], "uti_password": rows[0][2], "uti_token": rows[0][3], "uti_online": rows[0][4], "uti_token_expiration":rows[0][5]})
+
                     except (Exception, psycopg2.DatabaseError) as error:
                         print(error)
-                        return jsonify({"Code": NOT_FOUND_CODE, "Erro": "Utilizador não encontrado"})
+                        return jsonify({"Code": NOT_FOUND_CODE, "Erro": "Erro no update"})
         conn.close()
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
